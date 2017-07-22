dc17fcee488e5a827136533837acf15e021223a3
SILVER <silver199420@gmail.com>
2017-07-22 18:05:55 -0500

    Se creó la inferfaz para el usuario con su NavigationDrawer, se añadió la navegación en las páginas, se crearon los íconos para el NavigationDrawer

--- app/src/main/java/catchmyshift/catchmyshift/MainActivity.java
+++ app/src/main/java/catchmyshift/catchmyshift/MainActivity.java
@@ -1,14 +1,39 @@
 package catchmyshift.catchmyshift;
 
+import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnSearchJob, btnPublishVacancy;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+
+        btnSearchJob = (Button) findViewById(R.id.button_buscartrabajo);
+        btnPublishVacancy = (Button) findViewById(R.id.button_publicvacante);
+
+        btnSearchJob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent().setClass(getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnPublishVacancy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v, "Función pendiente", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
+
 }
