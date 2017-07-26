77d1aba645cd122bd85c31b4805e8b0e97d50435
SILVER <silver199420@gmail.com>
2017-07-26 03:36:07 -0500

    Se agrego el Login para la Empresa

---
+++ app/src/main/java/catchmyshift/catchmyshift/LoginActivityCompany.java
@@ -0,0 +1,26 @@
+package catchmyshift.catchmyshift;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivityCompany extends AppCompatActivity {
+    private Button btnLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_company);
+
+        btnLogin = (Button) findViewById(R.id.button_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent().setClass(getApplicationContext(), UserActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
