5b7ebadbc7df268e410de2e4f674ede504ff7356
mely_casta_eda <melycastp@gmail.com>
2017-08-06 17:07:58 -0500

    Crear cuenta empresa

--- app/src/main/java/catchmyshift/catchmyshift/LoginActivityCompany.java
+++ app/src/main/java/catchmyshift/catchmyshift/LoginActivityCompany.java
@@ -6,20 +6,36 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivityCompany extends AppCompatActivity {
     private Button btnLogin;
+    private TextView txtCreateAccount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_company);
 
         btnLogin = (Button) findViewById(R.id.button_login);
+        txtCreateAccount = (TextView) findViewById(R.id.textview_createaccount);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try{
                     Intent intent = new Intent().setClass(getApplicationContext(),CompanyActivity.class);
+                    startActivity(intent);
+                }
+
+                catch (Exception e){
+                    Log.e("JMMC",e.getMessage());
+                }
+            }
+        });
+        txtCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    Intent intent = new Intent().setClass(getApplicationContext(),CreateCompanyActivity.class);
                     startActivity(intent);
                 }
 
