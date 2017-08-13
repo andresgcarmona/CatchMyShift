1e27d702cfda26a0773d61a9db1d06eb2a08373e
Silver <silver199420@gmail.com>
2017-08-12 20:10:39 -0500

    Se añadio la vista para crear un usuario, se modificó el Navigation Drawer

---
+++ app/src/main/java/catchmyshift/catchmyshift/CreateUserActivity.java
@@ -0,0 +1,16 @@
+package catchmyshift.catchmyshift;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.transition.Fade;
+import android.transition.Slide;
+
+public class CreateUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_user);
+    }
+
+}
--- app/src/main/java/catchmyshift/catchmyshift/LoginActivity.java
+++ app/src/main/java/catchmyshift/catchmyshift/LoginActivity.java
@@ -5,15 +5,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
     private Button btnLogin;
+    private TextView txtCreateAcc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
         btnLogin = (Button) findViewById(R.id.button_login);
+        txtCreateAcc =(TextView)findViewById(R.id.text_createAccoutn);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -21,6 +24,14 @@ public class LoginActivity extends AppCompatActivity {
                 Intent intent = new Intent().setClass(getApplicationContext(), UserActivity.class);
                 startActivity(intent);
                 finish();
+            }
+        });
+
+        txtCreateAcc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent().setClass(getApplicationContext(),CreateUserActivity.class);
+                startActivity(intent);
             }
         });
     }
