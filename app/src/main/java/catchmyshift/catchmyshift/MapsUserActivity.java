9c8f1ac6e863ebdfe7f05a78687815e8a0535e86
Silver <silver199420@gmail.com>
2017-08-12 23:25:33 -0500

    Se modifico la interfaz del perfil de usuario, se añadio el mapa, falta mostrar su localiación

--- app/src/main/java/catchmyshift/catchmyshift/MapsUserActivity.java
+++ app/src/main/java/catchmyshift/catchmyshift/MapsUserActivity.java
@@ -1,9 +1,12 @@
 package catchmyshift.catchmyshift;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -16,14 +19,15 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsUserActivity extends AppCompatActivity  implements OnMapReadyCallback {
+public class MapsUserActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    private View currentView ;
+    private View currentView;
     private Context context;
     private String data;
 
     GoogleMap map;
     MyMethods myMethods = new MyMethods();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,11 +42,9 @@ public class MapsUserActivity extends AppCompatActivity  implements OnMapReadyCa
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (map.getMapType() == GoogleMap.MAP_TYPE_NORMAL)
-                {
+                if (map.getMapType() == GoogleMap.MAP_TYPE_NORMAL) {
                     map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-                }
-                else{
+                } else {
                     map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
                 }
 
@@ -57,13 +59,26 @@ public class MapsUserActivity extends AppCompatActivity  implements OnMapReadyCa
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
-        LatLng pp = new LatLng(16.860959782368866,-99.88278150558472);
+        LatLng pp = new LatLng(16.860959782368866, -99.88278150558472);
         MarkerOptions option = new MarkerOptions();
         option.position(pp).title("Empresa de Desarrollo");
         float zoomLevel = (float) 16.0;
         map.addMarker(option);
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(pp,zoomLevel));
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(pp, zoomLevel));
 
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        else {
+            map.setMyLocationEnabled(true);
+        }
         data = getString(R.string.title_Success);
         myMethods.Success(currentView, data, context).show();
     }
--- app/src/main/java/catchmyshift/catchmyshift/ProfileFragment.java
+++ app/src/main/java/catchmyshift/catchmyshift/ProfileFragment.java
@@ -1,12 +1,25 @@
 package catchmyshift.catchmyshift;
 
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 
 /**
@@ -14,17 +27,44 @@ import android.view.ViewGroup;
  */
 public class ProfileFragment extends Fragment {
 
-    private FragmentManager manager;
-    public ProfileFragment() {
-        // Required empty public constructor
-    }
-
+    MapView mMapView;
+    private GoogleMap googleMap;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_profile, container, false);
+        final View v = inflater.inflate(R.layout.fragment_profile, container, false);
+        mMapView = (MapView) v.findViewById(R.id.mapView);
+        mMapView.onCreate(savedInstanceState);
+
+        mMapView.onResume(); // needed to get the map to display immediately
+
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        mMapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap mMap) {
+                googleMap = mMap;
+                if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                else {
+                    mMap.setMyLocationEnabled(true);
+                }
+            }
+        });
         return v;
     }
 
