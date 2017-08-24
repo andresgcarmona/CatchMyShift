2212d7c10a19fa732f5eee2dceece8834fa96269
Silver <silver199420@gmail.com>
2017-08-24 00:24:25 -0500

    Se implementó el metodo para el acceso a la camara y galeria del dispositivo, pendiente modificar los dialogos indicadores

--- app/src/main/java/catchmyshift/catchmyshift/activity/EditUserActivity.java
+++ app/src/main/java/catchmyshift/catchmyshift/activity/EditUserActivity.java
@@ -1,36 +1,38 @@
 package catchmyshift.catchmyshift.activity;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.app.Activity;
+
+import android.Manifest;
 import android.app.Dialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.squareup.picasso.Picasso;
+import com.kosalgeek.android.photoutil.CameraPhoto;
 
-import org.w3c.dom.Text;
+import java.io.ByteArrayOutputStream;
 
 import butterknife.BindString;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import catchmyshift.catchmyshift.R;
-import catchmyshift.catchmyshift.fragment.PaymentFragment;
-import catchmyshift.catchmyshift.fragment.ProfileFragment;
 import de.hdodenhof.circleimageview.CircleImageView;
+
 
 public class EditUserActivity extends AppCompatActivity {
 
@@ -39,6 +41,9 @@ public class EditUserActivity extends AppCompatActivity {
     Dialog myDialog;
     Button cameraOption, galleryOpcion;
     TextView cancelOption;
+    private Bitmap bitmap;
+    private final int GALLERY_REQUEST = 0;
+    private final int CAMERA_REQUEST=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,14 +70,35 @@ public class EditUserActivity extends AppCompatActivity {
         cameraOption.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                myDialog.hide();
+
+
+                if (checkPermissionCAM()) {
+
+                    Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(cameraIntent, CAMERA_REQUEST);
+                    myDialog.hide();
+
+                } else {
+                    requestPermissionCAM();
+                }
             }
         });
 
         galleryOpcion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                myDialog.hide();
+
+                if (checkPermission()) {
+
+                    Intent intent = new Intent();
+                    intent.setType("image/*");
+                    intent.setAction(Intent.ACTION_GET_CONTENT);
+                    startActivityForResult(Intent.createChooser(intent, "Select Picture"),GALLERY_REQUEST);
+                    myDialog.hide();
+
+                } else {
+                    requestPermission();
+                }
             }
         });
 
@@ -85,4 +111,87 @@ public class EditUserActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if(requestCode == GALLERY_REQUEST && resultCode == RESULT_OK && null !=data && data.getData()!=null){
+            Uri filePath = data.getData();
+            try {
+                //Getting the Bitmap from Gallery
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                Toast.makeText(this, ""+bitmap, Toast.LENGTH_SHORT).show();
+                //Setting the Bitmap to ImageView
+                imageUser.setImageBitmap(bitmap);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        else{
+            if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK ){
+                Bundle extras = data.getExtras();
+                bitmap = (Bitmap) extras.get("data");
+                imageUser.setImageBitmap(bitmap);
+            }
+        }
+    }
+
+    public String getStringImage(Bitmap bitmap){
+        Log.i("MyHitesh",""+ bitmap);
+        ByteArrayOutputStream baos = new  ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG,100, baos);
+        byte [] b=baos.toByteArray();
+        String temp= Base64.encodeToString(b, Base64.DEFAULT);
+
+
+        return temp;
+    }
+
+
+    private void requestPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            Toast.makeText(this, " Please allow this permission in App Settings.", Toast.LENGTH_LONG).show();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, GALLERY_REQUEST);
+        }
+    }
+
+    private boolean checkPermission() {
+        int result = ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (result == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    private void requestPermissionCAM() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+            Toast.makeText(this, " Please allow this permission in App Settings.", Toast.LENGTH_LONG).show();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAMERA_REQUEST);
+        }
+    }
+
+    private boolean checkPermissionCAM() {
+        int result = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        if (result == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case GALLERY_REQUEST:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Permission Granted Successfully! ", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(this, "Permission Denied 🙁 ", Toast.LENGTH_LONG).show();
+                }
+                break;
+        }
+    }
 }
