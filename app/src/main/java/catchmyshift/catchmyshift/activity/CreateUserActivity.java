8576861fc18e08265fe0d011b3ede0ed66eed193
Silver <silver199420@gmail.com>
2017-08-29 23:25:45 -0500

    Se agregó validación para crear el usuario

--- app/src/main/java/catchmyshift/catchmyshift/activity/CreateUserActivity.java
+++ app/src/main/java/catchmyshift/catchmyshift/activity/CreateUserActivity.java
@@ -2,7 +2,9 @@ package catchmyshift.catchmyshift.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -13,7 +15,10 @@ import catchmyshift.catchmyshift.utilities.MyMethods;
 public class CreateUserActivity extends AppCompatActivity {
 
     @BindView(R.id.textEmailAddress) EditText emailTxt;
-
+    @BindView(R.id.textCellphoneNumber) EditText phoneTxt;
+    @BindView(R.id.textPassword) EditText passwordTxt;
+    @BindView(R.id.textConfirmPassword) EditText confirmPassTxt;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +28,38 @@ public class CreateUserActivity extends AppCompatActivity {
 
     @OnClick(R.id.btnAcceptCreate)
     public void CrearUsuario(){
+        try {
+            Log.e("JMMC_EMAIL:",">"+emailTxt.getText()+"<");
+            if(emailTxt.getText().toString().equals("")){
+                MyMethods.InfoDialog(CreateUserActivity.this, "Warning","You must set an email address").show();
+            }
+            else{
+                String email = emailTxt.getText().toString().trim();
+                if(email.matches(emailPattern)){
+                    if(!passwordTxt.getText().toString().equals(confirmPassTxt.getText().toString())){
+                        MyMethods.InfoDialog(CreateUserActivity.this, "Warning","Your password and confirm password field must be equals").show();
+                    } else{
+                        if(!passwordTxt.getText().toString().equals("")){
+
+                            //EN ESTA PARTE IRÁ EL MÉTODO PARA CREAR EL USUARIO
+                            CreateUser();
+                        }
+                        else {
+                            MyMethods.InfoDialog(CreateUserActivity.this, "Warning","Set a password").show();
+                        }
+                    }
+                }
+                else{
+                    MyMethods.InfoDialog(CreateUserActivity.this, "Warning","Set a valid email address").show();
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+    public void CreateUser(){
         MyMethods.VerifyDialog(CreateUserActivity.this, emailTxt.getText().toString()).show();
     }
 }
