7e79d4f24fbb78e1845c2bc6dc2ad03a84322831
mely_casta_eda <melycastp@gmail.com>
2017-08-29 21:40:05 -0500

    datepicker en Education

--- app/src/main/java/catchmyshift/catchmyshift/activity/EducationActivity.java
+++ app/src/main/java/catchmyshift/catchmyshift/activity/EducationActivity.java
@@ -1,15 +1,74 @@
 package catchmyshift.catchmyshift.activity;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
+import java.util.Calendar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import catchmyshift.catchmyshift.R;
 
 public class EducationActivity extends AppCompatActivity {
+
+    @BindView(R.id.idanio_inicial)TextView anioInicial;
+    @BindView(R.id.idanio_final)TextView anioFinal;
+    private int mYear, mMonth, mDay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_education);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.idanio_inicial)
+    public void SetStartDate(){
+        // Get Current Date
+        final Calendar c = Calendar.getInstance();
+        mYear = c.get(Calendar.YEAR);
+        mMonth = c.get(Calendar.MONTH);
+        mDay = c.get(Calendar.DAY_OF_MONTH);
+
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                new DatePickerDialog.OnDateSetListener() {
+
+                    @Override
+                    public void onDateSet(DatePicker view, int year,
+                                          int monthOfYear, int dayOfMonth) {
+
+                        anioInicial.setText((monthOfYear + 1) + "/" + dayOfMonth + "/" + year);
+
+                    }
+                }, mYear, mMonth, mDay);
+        datePickerDialog.show();
+    }
+
+    @OnClick(R.id.idanio_final)
+    public void SetEndDate(){
+        // Get Current Date
+        final Calendar c = Calendar.getInstance();
+        mYear = c.get(Calendar.YEAR);
+        mMonth = c.get(Calendar.MONTH);
+        mDay = c.get(Calendar.DAY_OF_MONTH);
+
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                new DatePickerDialog.OnDateSetListener() {
+
+                    @Override
+                    public void onDateSet(DatePicker view, int year,
+                                          int monthOfYear, int dayOfMonth) {
+
+                        anioFinal.setText((monthOfYear + 1) + "/" + dayOfMonth + "/" + year);
+
+                    }
+                }, mYear, mMonth, mDay);
+        datePickerDialog.show();
     }
 }
