3316303d4e3935658c63da46210214da23b33867
Silver <silver199420@gmail.com>
2017-09-30 23:42:44 -0500

    Se agrego la vista para inicio de sesión con compañía

--- app/src/main/java/catchmyshift/catchmyshift/activity/LoginActivityCompany.java
+++ app/src/main/java/catchmyshift/catchmyshift/activity/LoginActivityCompany.java
@@ -8,43 +8,35 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import catchmyshift.catchmyshift.R;
 
 public class LoginActivityCompany extends AppCompatActivity {
-    private Button btnLogin;
-    private TextView txtCreateAccount;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_company);
-
-        btnLogin = (Button) findViewById(R.id.button_login);
-        txtCreateAccount = (TextView) findViewById(R.id.textview_createaccount);
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try{
-                    Intent intent = new Intent().setClass(getApplicationContext(),CompanyActivity.class);
-                    startActivity(intent);
-                }
-
-                catch (Exception e){
-                    Log.e("JMMC",e.getMessage());
-                }
-            }
-        });
-        txtCreateAccount.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try{
-                    Intent intent = new Intent().setClass(getApplicationContext(),CompaniesActivity.class);
-                    startActivity(intent);
-                }
-
-                catch (Exception e){
-                    Log.e("JMMC",e.getMessage());
-                }
-            }
-        });
+        ButterKnife.bind(this);
     }
+
+    @OnClick(R.id.button_login_comp)
+    public void Login(){
+        Intent intent = new Intent().setClass(getApplicationContext(),CompaniesActivity.class);
+        startActivity(intent);
+    }
+
+
 }
+/*
+BTN LOGIN
+    Intent intent = new Intent().setClass(getApplicationContext(),CompaniesActivity.class);
+    startActivity(intent);
+
+BTN CREATE ? <...revisar
+    Intent intent = new Intent().setClass(getApplicationContext(),CompanyActivity.class);
+    startActivity(intent);
+
+    */
